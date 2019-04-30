ebb54af3844ed7c2fb37f144ddde7cc74b9f55e1
Your Name <you@example.com>
2019-04-30 17:15:35 +0800

    front.py

--- jobplus/form.py
+++ jobplus/form.py
@@ -1,22 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import Length, Email, EqualTo, Required
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, IntegerField, TextAreaField, SelectField
-
+from jobplus.models import db,User
 
 class RegisterForm(FlaskForm):
-    name = StringField('???', validators=[Required(), Length(3, 24)])
-    email = StringField('??', validators=[Required(), Email()])
-    password = PasswordField('??', validators=[Required(), Length(6, 24)])
-    repeat_password = PasswordField('????', validators=[Required(), EqualTo('password')])
-    submit = SubmitField('??')
+    name = StringField('用户名', validators=[Required(), Length(3, 24)])
+    email = StringField('邮箱', validators=[Required(), Email()])
+    password = PasswordField('密码', validators=[Required(), Length(6, 24)])
+    repeat_password = PasswordField('重复密码', validators=[Required(), EqualTo('password')])
+    submit = SubmitField('提交')
 
     def validate_username(self, field):
         if User.query.filter_by(name=field.data).first():
-            raise ValidationError('??????')
+            raise ValidationError('用户名已存在')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
-            raise ValidationError('??????')
+            raise ValidationError('邮箱已存在')
 
     def create_user(self):
         user = User(name=self.name.data,
@@ -25,3 +25,19 @@ class RegisterForm(FlaskForm):
         db.session.add(user)
         db.session.commit()
         return user
+
+
+class LoginForm(FlaskForm):
+    email = StringField('邮箱', validators=[Required(), Email()])
+    password = PasswordField('密码', validators=[Required(), Length(6, 24)])
+    remember_me = BooleanField('重复')
+    submit = SubmitField('提交')
+
+    def validate_email(self, field):
+        if field.data and not User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱不存在或未注册')
+
+    def validate_password(self, field):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user and not user.check_password(field.data):
+            raise ValidationError('密码错误')
--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,8 +1,52 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app
 from flask_login import login_user, logout_user, login_required
+from jobplus.form import RegisterForm,LoginForm
 
 front = Blueprint('front', __name__)
 
 @front.route('/')
 def index():
     return render_template('index.html')
+
+@front.route('/userregister',methods=['GET','POST'])
+def userregister():
+	form = RegisterForm
+	if form.validate_on_submit():
+	form.create_user()
+	flash('user register', 'success')
+	return redirect(url_for('.login'))
+	return render_template('userregister.html',form=form)
+
+@front.route('/companyregister',methods=['GET','POST'])
+def companyregister():
+	form = RegisterForm()
+	form.name.label = u'company'
+    if form.validate_on_submit():
+        company_user = form.create_user()
+        company_user.role = User.ROLE_COMPANY
+        db.session.add(company_user)
+        db.session.commit()
+        flash('company register', 'success')
+        return redirect(url_for('.login'))
+	return render_template('companyregister.html',form=form)
+
+@front.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm()
+    if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    login_user(user, form.remember_me.data)
+    next = 'user.profile'
+    if user.is_admin:
+        next = 'admin.index'
+    elif user.is_company:
+        next = 'company.profile'
+    return redirect(url_for(next))
+	return render_template('login.html',form=form)
+
+@front.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('???????', 'success')
+    return redirect(url_for('.index'))
